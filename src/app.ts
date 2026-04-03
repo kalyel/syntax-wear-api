21f2f5591a5928c0f07704adcf338ae0a0ae30f7
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2026-04-02 22:20:56 -0300

    feat: configura swagger e scalar

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,89 @@
 // Require the framework and instantiate it
 
 // ESM
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import helmet from '@fastify/helmet'
-import 'dotenv/config'
-import { uptime } from 'node:process'
-import { timeStamp } from 'node:console'
-import productRoutes from './routes/products.routes'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import "dotenv/config";
+import { uptime } from "node:process";
+import { timeStamp } from "node:console";
+import productRoutes from "./routes/products.routes";
+import swagger from "@fastify/swagger";
+import scalar from "@scalar/fastify-api-reference";
 
-const PORT = parseInt(process.env.PORT ?? '3000')
+const PORT = parseInt(process.env.PORT ?? "3000");
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 fastify.register(cors, {
-    origin: true,
-    credentials: true
-})
+  origin: true,
+  credentials: true,
+});
 fastify.register(helmet, {
-    contentSecurityPolicy: false
-})
+  contentSecurityPolicy: false,
+});
 
-fastify.register(productRoutes, { prefix: '/products' })
+fastify.register(swagger, {
+  openapi: {
+    openapi: "3.0.0",
+    info: {
+      title: "Syntax Wear API",
+      description: "API para o e-commerce Syntax Wear",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+        description: "Servidor de desenvolvimento",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          description: "Autenticação via token JWT",
+        },
+      },
+    },
+  },
+});
+
+fastify.register(scalar,{
+  routePrefix: "/api-docs",
+  configuration: {
+    theme: 'default',
+  }
+});
+
+fastify.register(productRoutes, { prefix: "/products" });
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
+fastify.get("/", async (request, reply) => {
   return {
-    message: 'E-commerce Syntax Wear API',
-    version: '1.0.0',
-    status: 'running'
-  }
-})
+    message: "E-commerce Syntax Wear API",
+    version: "1.0.0",
+    status: "running",
+  };
+});
 
-fastify.get('/health', async (request, reply) => {
+fastify.get("/health", async (request, reply) => {
   return {
-    status: 'ok',
-    timeStamp: new Date().toISOString()
-  }
-})
+    status: "ok",
+    timeStamp: new Date().toISOString(),
+  };
+});
 
 // Run the server!
 fastify.listen({ port: PORT }, function (err, address) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
   // Server is now listening on ${address}
-})
+});
 
 export default fastify;
